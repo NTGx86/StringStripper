c805d078c76e91c0338c75adc632d70f440b2645
Nathan Madak <nathan.madak@gmail.com>
2023-06-01 16:55:49 -0400

    Refined comments and Code

--- README.md
+++ README.md
@@ -1,24 +1,27 @@
 # StringStripper
 Title:             StringStripper
-Verison:           1.1
+Verison:           1.2
 Author:            NTGx86
-Last Modified:     May 31 2023
+Last Modified:     June 1 2023
 
 
 Short Description: 
 
-This script filters the output from Strings.exe (Forensics/Malware Analysis tool). 
+This script filters the output from Strings.exe (Forensics/Malware Analysis tool). Attempting to remove the vast
+majority of strings that will not likely be relevant to a malware analyst. 
 
 Detailed Description: 
 
 For those familiar with Strings.exe you're surely aware that more often than not the number of lines outputted
 can be more than anyone wants to manual sift through. Hence, I've created this tool. As of now it first searches 
 for strings or lines that contain possible file extensions or urls and then it searches for any other 
-possible relevant strings
+possible relevant strings of either a shorter length or a longer length. Then lastly includes strings that are
+over 15 chars in length, as there's a higher chance they may be relevant to a malware analyst. 
 
 Future Plans:
 
 1) Change the functionality so it can work from the command-line.
+2) Continue to test the script against real live malware and refine the filtering parameters. 
 
 Final Notes: 
 
--- StringStripper.py
+++ StringStripper.py
@@ -1,24 +1,27 @@
 '''
 Title:             StringStripper
-Verison:           1.1
+Verison:           1.2
 Author:            NTGx86
-Last Modified:     May 31 2023
+Last Modified:     June 1 2023
 
 
 Short Description: 
 
-This script filters the output from Strings.exe (Forensics/Malware Analysis tool). 
+This script filters the output from Strings.exe (Forensics/Malware Analysis tool). Attempting to remove the vast
+majority of strings that will not likely be relevant to a malware analyst. 
 
 Detailed Description: 
 
 For those familiar with Strings.exe you're surely aware that more often than not the number of lines outputted
 can be more than anyone wants to manual sift through. Hence, I've created this tool. As of now it first searches 
 for strings or lines that contain possible file extensions or urls and then it searches for any other 
-possible relevant strings
+possible relevant strings of either a shorter length or a longer length. Then lastly includes strings that are
+over 15 chars in length, as there's a higher chance they may be relevant to a malware analyst. 
 
 Future Plans:
 
 1) Change the functionality so it can work from the command-line.
+2) Continue to test the script against real live malware and refine the filtering parameters. 
 
 Final Notes: 
 
@@ -53,74 +56,84 @@ try:
     # Open "final_lines.txt" in write mode
     # check for any mathces and write them to the file (basically what you might grep for) 
     with open('final_lines.txt', 'w') as f:
+        ''' WRITING LINES THAT CONTAIN ANY POSSIBLE FILE EXTENSIONS OR URLs '''
         f.write('='*75)
-        f.write('\nWRITING ALL LINES THAT CONTAIN AN: .exe or www. or .com or .dll\n')
+        f.write('\nWRITING LINES THAT CONTAIN ANY POSSIBLE FILE EXTENSIONS OR URLs\n')
         f.write('='*75)  
-        f.write('\n')
+        f.write('\n')        
         for line in lines:
-            if ('.exe' in line) or ('www.' in line) or ('.com' in line) or ('.dll' in line):
+            possibleFileExtMatch = re.findall(r'\.\w{3}', line)
+            if possibleFileExtMatch:
                 f.write(line)
                 strings.add(line)
                 additionalLineCount += 1
         f.write('\n') 
-      
-        ''' searching some more '''
-        f.write('='*75)
-        f.write('\nWRITING ADDITIONAL LINES THAT CONTAIN ANY POSSIBLE FILE EXTENSIONS OR URLs\n')
-        f.write('='*75)  
-        f.write('\n')        
-        for line in lines:
-            if not ('.exe' in line) or ('www.' in line) or ('.com' in line) or ('.dll' in line):
-                # pulling out any other lines with possible file extensions
-                possibleFileExtMatch = re.findall(r'\.\w{3}', line)
-                if possibleFileExtMatch:
-                    f.write(line) 
-                    strings.add(line)
-                    additionalLineCount += 1
-
-        f.write('\n') 
         f.write('='*75) 
-        f.write('\n')                 
+        f.write('\n')      
  
     ''' Start of Regex madness created with the help of ChatGPT (will refine at some point)  '''
-    # Filter out invalid lines based on these criteria:
-    # - Line length must at least 4 characters
-    # - Line must not contain 3 or more consecutive non-alphanumeric characters
-    valid_lines = [line for line in lines if len(line.rstrip()) >= 4 and not re.search(r'[^\w\d]{3}', line)]
-
-    # Filter out lines that contain 2 or more occurrences (consecutive or non-consecutive) 
-    # of the specified special characters using a regular expression with a positive lookahead assertion 
-    # to check for the presence of the characters, followed by the same characters again.
-    final_lines = [line for line in valid_lines if not re.search(r'[%=+;"@#$\]\[\}\{]{2,}', line)]
+    # Define a list comprehension to filter the input 'lines' list.
+    # The filtered list 'valid_lines' only includes lines that:
+    # 1) Are 4 characters or longer (after leading and trailing whitespace is removed).
+    # 2) Do not contain three consecutive non-alphanumeric characters.
+    valid_lines = [
+        line.strip() for line in lines 
+        if len(line.strip()) >= 4 and not re.search(r'[^\w\d]{3}', line)
+    ]
     
-
-    ''' Lines below perform additional filtering based on number of special chars and length of the string '''
-    ''' The combination of length & about of chars to search for is based off my estimations '''
-    final_lines2 = [line for line in final_lines if not (len(line.strip()) == 5 and not re.search(r'[%=+;"@#$\]\[\}\{]{2,}', line))]
-
-    final_lines3 = [line for line in final_lines2 if not (len(line.strip()) == 6 and not re.search(r'[%=+;"@#$\]\[\}\{]{3,}', line))]
-
-    final_lines4 = [line for line in final_lines3 if not (len(line.strip()) == 7 and not re.search(r'[%=+;"@#$\]\[\}\{]{3,}', line))]
-
-    final_lines5 = [line for line in final_lines4 if not (len(line.strip()) == 8 and not re.search(r'[%=+;"@#$\]\[\}\{]{3,}', line))]
-
-    final_lines6 = [line for line in final_lines5 if not (len(line.strip()) == 9 and not re.search(r'[%=+;"@#$\]\[\}\{]{3,}', line))]
-
-    final_lines7 = [line for line in final_lines6 if not (len(line.strip()) == 10 and not re.search(r'[%=+;"@#$\]\[\}\{]{3,}', line))]
-            
-    final_lines8 = [line for line in final_lines7 if not (len(line.strip()) == 11 and not re.search(r'[%=+;"@#$\]\[\}\{]{3,}', line))]
+    # Define a second list comprehension to further filter 'valid_lines'.
+    # The resulting list 'short_lines' only includes lines that:
+    # 1) Are shorter than 6 characters.
+    # 2) Do not contain at least two consecutive characters from the set '%=+;"@#$][}{'. 
+    short_lines = [
+        line for line in valid_lines 
+        if len(line) < 6 and not re.search(r'[%=+;"@#$\]\[\}\{]{2,}', line)
+    ]
+    
+    # Define a third list comprehension to further filter 'valid_lines'.
+    # The resulting list 'long_lines' only includes lines that:
+    # 1) Are 7 characters or longer.
+    # 2) Do not contain at least three consecutive characters from the set '%=+;"@#$][}{'. 
+    long_lines = [
+        line for line in valid_lines 
+        if len(line) >= 7 and not re.search(r'[%=+;"@#$\]\[\}\{]{3,}', line)
+    ]
 
     ''' Open "final_lines.txt" in append mode '''
     with open('final_lines.txt', 'a') as f:
-        f.write('WRITING ALL OTHER STRIPPED LINES\n')
+        f.write('WRITING SHORT & LONG STRIPPED LINES\n')
         f.write('='*75)
-        f.write('\n')
-        for line in final_lines8:
-            if line not in strings:
-                # Write each non-duplicate line in final_lines8 to the new file
-                f.write(line)
-                allOtherCount += 1
+        f.write('\n') 
         
+        ''' long lines  '''
+        for line in long_lines:
+            if (line not in strings):
+                # Write each non-duplicate long line to the file
+                f.write(line + '\n')
+                strings.add(line)
+                allOtherCount += 1 
+                
+        ''' short lines  '''
+        for line in short_lines:
+            if (line not in strings):
+                # Write each non-duplicate short line to the file
+                f.write(line + '\n')
+                strings.add(line)
+                allOtherCount += 1 
+
+        f.write('\n')
+        f.write('='*75)
+        f.write('\nWRITING ALL REALLY LONG UNALTERED LINES (>15 IN LENGTH).\n')
+        f.write('='*75)  
+        f.write('\n') 
+        
+        ''' really long lines  '''
+        for line in lines:
+            # write all longest lines not already written
+            if (line not in strings) and (len(line) > 15):
+                f.write(line)
+                strings.add(line)
+                allOtherCount += 1             
         f.write('\n')
         f.write('='*75)
         f.write('\nALL STRIPPED LINES PRINTED. SCRIPT COMPLETE.\n')
@@ -128,7 +141,7 @@ try:
     
     ''' Script complete printing results to screen '''    
     print("Number of lines in file before filtering: ", len(lines))
-    print("Number of lines after filtering:          ", allOtherCount + additionalLineCount)
+    print("Number of lines after filtering:          ", len(strings))
     
     print('\nOutput File Created: final_lines.txt')
     
